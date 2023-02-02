37609fab20642fb0712feb680b0c2edbe3bbb211
Mikhail Scherbina <awarebayes@gmail.com>
2023-02-02 16:18:33 +0300

    misc

--- PASS_transreid/run_pass_vit_small_Market.sh
+++ PASS_transreid/run_pass_vit_small_Market.sh
@@ -1,4 +1,4 @@
 #Single GPU
-python train.py --config_file configs/market/vit_small.yml MODEL.PRETRAIN_PATH "/userhome/zhukuan/PASS-reID/pre_trained_model/pass_vit_small_full.pth" OUTPUT_DIR './logs/market/pass_vit_small_full_cat' SOLVER.SEED 42 MODEL.DEVICE_ID '("4")'
+python train.py --config_file configs/market/vit_small.yml MODEL.PRETRAIN_PATH "/home/mscherbina/Documents/github_repos/PASS-reID/models/pass_transreid_vit_small.pth" OUTPUT_DIR './logs/market/pass_vit_small_full_cat' SOLVER.SEED 42 MODEL.DEVICE_ID '("0")'
 
 
